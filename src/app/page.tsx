33a9f138b6f6d124245a89f8cf54f28b3d3df59f
Abbas khalil <khalilabbas389@gmail.com>
2024-06-12 19:25:22 +0530

    changes in main route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
-import Hero from "./components/Hero"
-import Sidebar from "./components/Sidebar"
+import Hero from "./components/Hero";
+import Sidebar from "./components/Sidebar";
 
 const page = () => {
   return (
-
     <main className="flex">
-      <div className="w-1/6 text-white h-screen "><Sidebar /></div>
+      <div className="w-1/6 text-white h-screen ">
+        <Sidebar />
+      </div>
       <div className="w-5/6 bg-blue-500 h-screen">
-    
-    <Hero />
-    
-    </div>
+        <Hero />
+      </div>
     </main>
-  )
-}
-export default page
+  );
+};
+export default page;
