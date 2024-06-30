4267676a0072e9b81d74ea9462a2f8b272a29ed2
Abbas khalil <khalilabbas389@gmail.com>
2024-06-30 04:23:55 -0700

    contact section add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
       animation: {
         scroll:
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
@@ -32,8 +36,19 @@ const config: Config = {
       },
     },
   },
-  plugins: [
+
+   plugins: [
     // addVariablesForColors,
+    function addVariablesForColors({ addBase, theme }: any) {
+      let allColors = flattenColorPalette(theme("colors"));
+      let newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+      );
+     
+      addBase({
+        ":root": newVars,
+      });
+    },
     function ({ matchUtilities, theme }: any) {
       matchUtilities(
         {
@@ -68,5 +83,10 @@ const config: Config = {
       });
     }
   ],
+
+
 };
+
+
+
 export default config;
