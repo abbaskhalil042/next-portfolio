19278245e0cab731468aa7542c07c32b1c8cbe09
Abbas khalil <khalilabbas389@gmail.com>
2024-06-30 02:28:43 -0700

    work section add.

--- src/app/components/HowIWorked.tsx
+++ src/app/components/HowIWorked.tsx
@@ -3,18 +3,23 @@ import React from "react";
 
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "./ui/canvas-reveal-effect";
+import { Code2Icon } from "lucide-react";
+import { Sparkle } from "./sparkle";
 
  function HowIWorked() {
   return (
     <>
-      <div className="py-20 flex flex-col lg:flex-row items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-8">
-        <Card title="Sheetal is Nisha" icon={<AceternityIcon />}>
+    <div>
+        <Sparkle Title="How I Worked" />
+    </div>
+      <div className=" flex flex-col lg:flex-row items-center justify-center bg-white dark:bg-black w-full gap-4 mx-auto px-8">
+        <Card title="step first" icon={<Code2Icon size={60}/>}>
           <CanvasRevealEffect
             animationSpeed={5.1}
             containerClassName="bg-emerald-900"
           />
         </Card>
-        <Card title="Nisha is Munni" icon={<AceternityIcon />}>
+        <Card title="step second" icon={<Code2Icon size={60}/>}>
           <CanvasRevealEffect
             animationSpeed={3}
             containerClassName="bg-black"
@@ -27,7 +32,7 @@ import { CanvasRevealEffect } from "./ui/canvas-reveal-effect";
           {/* Radial gradient for the cute fade */}
           <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
         </Card>
-        <Card title="Munni is Aditi" icon={<AceternityIcon />}>
+        <Card title="step third" icon={<Code2Icon size={60}/>}>
           <CanvasRevealEffect
             animationSpeed={3}
             containerClassName="bg-sky-600"
